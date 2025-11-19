249d8f6c9300e2f24a831084da6b1f464897ed6e
tasnimesahli <tasnimsahli97@gmail.com>
2025-11-19 09:18:33 +0100

    Delete js-aller-plus-loin/les-chapitre

--- js-aller-plus-loin/les-chapitre/chapitre-1/script.js
+++
@@ -1,8 +0,0 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.json())
-  .then(data => {
-    console.log("Données reçues :", data);
-  })
-  .catch(error => {
-    console.error("Erreur lors de la récupération :", error);
-  });
